367e5c66fb2c3d6793972a1cc2972d666c21a8ca
Josiah Smith <jsmith2@mines.edu>
2022-01-21 11:47:37 -0700

    Begin implementing truth table

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -8,18 +8,27 @@
 const int PIN_A = 2;
 const int PIN_B = 3;
 
+
 int a, b;
  
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  
   pinMode(PIN_A, INPUT_PULLUP);
   pinMode(PIN_B, INPUT_PULLUP);  
 
 }
 
 void loop() {
+  static byte lastState = 0;
   // put your main code here, to run repeatedly:
   a = digitalRead(PIN_A);
   b = digitalRead(PIN_B);
-  Serial.println(a, b);
+  
+  
+  Serial.print(a);
+  Serial.print(" ");
+  Serial.println(b);
+  delay(1);
 }

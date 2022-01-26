8e9eb8429812a031f1cc9ea381481851a121698e
Josiah Smith <jsmith2@mines.edu>
2022-01-26 11:52:51 -0700

    Add interupts, and smoothing to fix bounce error

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -1,7 +1,7 @@
 /*
  * Wiring:
  *   CLK (A) -> D2
- *   DT (B) -> D3
+ *   DT (B) -> D4
  *   GND -> GND
  */
 
@@ -10,72 +10,34 @@
 #define ROT_CNTCLK 0b10
 
 const int PIN_A = 2;
-const int PIN_B = 3;
+const int PIN_B = 4;
 
+int encCounts = 0;
 
-//int a, b;
-byte a,b;
-
-int evaluateState(byte vals){
-  int cnt;
-  
-  switch(vals){
-    case ROT_CLK:
-      cnt = 1;
-      break;
-    case ROT_CNTCLK:
-      cnt = -1;
-      break;
-    case ROT_NOCHNG:
-      cnt = 0;
-      Serial.println("Whaaaa");
-      break;
-    default:
-      Serial.println("Oof, bad");
-      
-  }
-
-  return cnt;
-}
- 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   
   pinMode(PIN_A, INPUT);
   pinMode(PIN_B, INPUT);  
-
+  attachInterrupt(digitalPinToInterrupt(PIN_A), updateState, CHANGE);
 }
 
-void loop() {
-  static byte lastA = 0b0;
-  static int counts = 0;
-//  int tmp;
-  
-  // put your main code here, to run repeatedly:
-  a = digitalRead(PIN_A);
-  b = digitalRead(PIN_B);
-//  state = (a<<1) | b;
-//  state = (digitalRead(PIN_A)<<1) | digitalRead(PIN_B);
+void updateState(){
+  byte a = digitalRead(PIN_A);
+  byte b = digitalRead(PIN_B);
 
-  if (a ^ lastA){
-    counts += 2*(a ^ b) - 1;
+  if (a == b){
+    encCounts -= 1;
+  } else {
+    encCounts += 1;
   }
-//  if (state != lastState){
-//    counts += evaluateState(state);
-//  }
-//
-lastA = a;
-//  lastState = state;
-//  Serial.print(a);
-//  Serial.print(" ");
-//  Serial.print(b);
-//  Serial.print(" ");
-//  Serial.println(state);
-//  Serial.print("Counts: ");
-//  Serial.println(counts);
-Serial.print(a^b);
-Serial.print(" ");
-Serial.println(counts);
+
   delay(1);
+}
+ 
+
+
+void loop() {
+  Serial.print("Counts: ");
+  Serial.println(encCounts);
 }

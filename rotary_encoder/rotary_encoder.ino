871075a175012ebe68fb23e4933085e5134bb953
Josiah Smith <jojorules47@gmail.com>
2022-01-27 20:19:09 -0700

    Save rotary encoder without interrupts

--- rotary_encoder/rotary_encoder.ino
+++ rotary_encoder/rotary_encoder.ino
@@ -5,77 +5,41 @@
  *   GND -> GND
  */
 
-#define ROT_NOCHNG 0b00
-#define ROT_CLK    0b01
-#define ROT_CNTCLK 0b10
-
 const int PIN_A = 2;
-const int PIN_B = 3;
+const int PIN_B = 4;
 
+byte a, b;
 
-//int a, b;
-byte a,b;
-
-int evaluateState(byte vals){
-  int cnt;
-  
-  switch(vals){
-    case ROT_CLK:
-      cnt = 1;
-      break;
-    case ROT_CNTCLK:
-      cnt = -1;
-      break;
-    case ROT_NOCHNG:
-      cnt = 0;
-      Serial.println("Whaaaa");
-      break;
-    default:
-      Serial.println("Oof, bad");
-      
-  }
-
-  return cnt;
-}
- 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  
-  pinMode(PIN_A, INPUT);
-  pinMode(PIN_B, INPUT);  
 
+  pinMode(PIN_A, INPUT);
+  pinMode(PIN_B, INPUT);
 }
 
 void loop() {
   static byte lastA = 0b0;
   static int counts = 0;
-//  int tmp;
-  
-  // put your main code here, to run repeatedly:
+
   a = digitalRead(PIN_A);
   b = digitalRead(PIN_B);
-//  state = (a<<1) | b;
-//  state = (digitalRead(PIN_A)<<1) | digitalRead(PIN_B);
 
-  if (a ^ lastA){
-    counts += 2*(a ^ b) - 1;
+  // Check if pin A has changed
+  if (a != lastA) {
+    // Update counts depending on phase
+    if (a == b) {
+      counts -= 1;
+    } else {
+      counts += 1;
+    }
+
+    Serial.print("Counts: ");
+    Serial.println(counts);
+    delay(1);
   }
-//  if (state != lastState){
-//    counts += evaluateState(state);
-//  }
-//
-lastA = a;
-//  lastState = state;
-//  Serial.print(a);
-//  Serial.print(" ");
-//  Serial.print(b);
-//  Serial.print(" ");
-//  Serial.println(state);
-//  Serial.print("Counts: ");
-//  Serial.println(counts);
-Serial.print(a^b);
-Serial.print(" ");
-Serial.println(counts);
+
+  // Save state of a
+  lastA = a;
+
   delay(1);
 }
